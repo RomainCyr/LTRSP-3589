ece22cfaa44f6f3b1b8cd89a4bd9d16fbec523bf
Romain CYRILLE <rcyrille@cisco.com>
2023-12-15 10:27:38 +0100

    Use docker cp for reset_config

--- reset_config.py
+++ reset_config.py
@@ -2,15 +2,15 @@ from genie import testbed
 from concurrent.futures import ThreadPoolExecutor,as_completed
 import logging
 from pyats.log import ScreenHandler
-from unicon.eal.dialogs import Statement, Dialog
+import os
+import subprocess
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
 
-CONFIG_PATH = "xrd-tools/cl_lab/configs/"
-HOST_PASSWORD = "cisco123"
+CONFIG_PATH = f"{os.getcwd()}/xrd/configs/"
 
 def reset_config(device):
     logger.info(f"{device.name}: Connecting")
@@ -18,27 +18,17 @@ def reset_config(device):
     logger.info(f"{device.name}: Clear configuration inconsistency")
     device.execute("clear configuration inconsistency")
     logger.info(f"{device.name}: Copying initial config to router")
-    password_dialog = Dialog(
-        [
-            Statement(
-                pattern=r"Password:",
-                action=lambda spawn: spawn.sendline(HOST_PASSWORD),
-                loop_continue=True,
-                continue_timer=False
-            )
-        ]
-    )
-    device.execute(f"scp cisco@172.40.0.1:{CONFIG_PATH}{device.name}_startup.cfg vrf MGMT /harddisk:/{device.name}_startup.cfg",reply= password_dialog)
+    filename = f"{device.name}_startup.cfg"
+    subprocess.run(f"docker cp {CONFIG_PATH}{filename} {device.name}:/harddisk:/",shell=True)
     logger.info(f"{device.name}:Commit replace initial config")
-    device.configure([f"load /harddisk:/{device.name}_startup.cfg",
+    device.configure([f"load /harddisk:/{filename}",
                       "commit replace"])
     logger.info(f"{device.name}: Clear configuration inconsistency")
     device.execute("clear configuration inconsistency")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logger.addHandler(ScreenHandler())
     testbed = testbed.load('testbed.yaml')
-
     with ThreadPoolExecutor(max_workers=10) as executor:
         threads = []
         for device in testbed:
